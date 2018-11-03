7da8c573e004a891820d1fe05054b27dab5bc423
killerwhale <alexandrisanas@gmail.com>
2018-11-03 17:51:31 +0200

    convert origin string

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 
 chrome.browserAction.onClicked.addListener(executor);
 
+
 function executor (request, sender, sendResponse) {
 
 	console.log(chrome.tabs);
@@ -28,56 +29,10 @@ function executor (request, sender, sendResponse) {
 
 	})
 
-
-	// send the message to the content to dropshie
-
-	// sendResponse({
-	// 	url,
-	// 	title 
-	// })
 }
 
-// function executor (tab) {
-	
-// 	// console.log(tab); // information about the tab in which the icon is clicked
-// 	// console.log("the url: ", tab.url); // the url of the page
 
 
-
-// 	// sends message to content.js
-// 	let msg = {
-// 		product: "​​https://www.amazon.co.uk/gp/product/B074C9TGFT",
-// 		url: "ebay uk",
-// 		template: "7c2f802d-05c0-4a29-8cc4-2f0492106dae"
-// 	}
-// 	// send message to the content.js of the tab with the corresponding id
-// 	chrome.tabs.sendMessage(tab.id, msg)
-// }
-
-
-chrome.runtime.onInstalled.addListener( function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		// console.log("The color is green")
-	});
-
-
-
-
-
-// the declarativeContent api allows to show the page action depending on the url or the page
-// 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-// 		chrome.declarativeContent.onPageChanged.addRules([
-// 				{
-// 					conditions: [ new chrome.declarativeContent.PageStateMatcher({
-// 							pageUrl: { hostEquals: 'developer.chrome.com' },
-// 						}) 
-// 					],
-// 					// enables the icon to the user
-// 					actions: [ new chrome.declarativeContent.ShowPageAction() ]
-// 				}
-// 			])
-// 	})
-});
 
 chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
 	console.log(req);
